3de9dd8b311926efae1f591acf6923e21d490ee3
Artem Pleschenko <darwin.ggl@gmail.com>
2020-04-05 00:01:52 +0300

    404 error handling

--- src/main.cr
+++ src/main.cr
@@ -5,7 +5,9 @@ cache = Cache.new
 
 get "/:country" do |env|
   env.response.content_type = "application/json"
-  cache.data[env.params.url["country"]].to_json
+  data = cache.data[env.params.url["country"]]?
+  halt env, status_code: 404, response: "#{env.params.url["country"]} not found" if data.nil? 
+  data.to_json
 end
 
 get "/" do |env|
